880fdd4604c0581fd076799c13fa58e4d17bee2a
Kevin <fabianuskevin147@gmail.com>
2025-02-06 20:22:58 +0700

    btnCreateTask: formnya uda tampil diatas button

--- script/main.js
+++ script/main.js
@@ -11,6 +11,26 @@ document.addEventListener('DOMContentLoaded', function () {
     event.preventDefault();
     addTask();
   });
+
+  const form = document.getElementById('taskForm');
+  const showFormButton = document.getElementById('showFormButton');
+
+  showFormButton.addEventListener('click', function () {
+    if (form.style.display === 'none' || form.style.display === '') {
+      form.style.display = 'block'; // Tampilkan form
+      setTimeout(() => {
+        form.classList.remove('opacity-0', 'translate-y-5');
+        form.classList.add('opacity-100', 'translate-y-0');
+      }, 10); // Tambahkan sedikit delay agar efek transisi bekerja
+    } else {
+      form.classList.remove('opacity-100', 'translate-y-0');
+      form.classList.add('opacity-0', 'translate-y-5');
+      setTimeout(() => {
+        form.style.display = 'none'; // Sembunyikan form setelah animasi
+      }, 300);
+    }
+  });
+
 });
 
 function showForm(overlay, form) {
@@ -37,25 +57,6 @@ function iniCategoryEventHandlers() {
   });
 }
 
-/* function cancelCreateCategory() {
-  const formCreateCategory = document.getElementById('formCreateCategory');
-  const overlay = document.querySelector('[node-create-form]');
-
-  overlay.classList.add('hidden');
-  formCreateCategory.classList.add('hidden');
-}
-
-function displayCreateCategory() {
-  const formCreateCategory = document.getElementById('formCreateCategory');
-  const btnCreateCategory = document.getElementById('btnCreateCategory');
-  const overlay = document.querySelector('[node-create-form]');
-  
-  btnCreateCategory.addEventListener('click', () => {
-    overlay.classList.remove('hidden');
-    formCreateCategory.classList.remove('hidden');
-  });
-}
- */
 function addTask() {
   const title = document.getElementById('inputTitle').value;
   const category = document.getElementById('inputCategory').value;
